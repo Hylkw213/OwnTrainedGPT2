a41e8b195dc1501c6f685e9d121e9ac535ac1777
gerben <gerben213@gmail.com>
2025-09-25 22:50:24 +0200

    added dataloader

--- train_gpt2.py
+++ train_gpt2.py
@@ -173,14 +173,64 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+# -----------------------------------------------------------------------------
+import tiktoken
 
-#---------------------------------------------------
-num_return_sequences = 5
-max_length = 30
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
 
-model = GPT.from_pretrained('gpt2')
-model.eval()
-model.to('cuda')
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+# -----------------------------------------------------------------------------
+device = "cuda"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+device = "cuda" # OVERRIDE
+
+train_loader = DataLoaderLite(B=4, T=32)
+# get logits
+model = GPT(GPTConfig())
+model.to(device)
+
+# optimize!
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)()
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+import sys; sys.exit(0)
 
 # prefix tokens
 import tiktoken
